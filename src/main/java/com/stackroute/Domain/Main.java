05436308488201d31e9d2c13edf1f7b7809e8b5e
khushbu <khushbuk316@gmail.com>
2019-05-14 17:24:28 +0530

    modified for task3

--- src/main/java/com/stackroute/Domain/Main.java
+++ src/main/java/com/stackroute/Domain/Main.java
@@ -17,16 +17,16 @@ public class Main {
 
 
             ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-            Movie movie = (Movie) context.getBean("movie");
-
-            System.out.println(movie);
-            Movie movie1 = (Movie) context.getBean("movie1");
-
-            System.out.println(movie1);
+//            Movie movie = (Movie) context.getBean("movie");
+//
+//            System.out.println(movie);
+//            Movie movie1 = (Movie) context.getBean("movie1");
+//
+//            System.out.println(movie1);
             Movie movie2 = (Movie) context.getBean("movie2");
 
             System.out.println(movie2);
-            System.out.println(movie==movie1);
+
 
         }
     }
